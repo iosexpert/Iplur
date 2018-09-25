3edf689e076a49a1a37599f52e26b628c3c0aeac
mac <mac@macs-mbp-2.dlink>
2018-09-25 11:43:19 +0530

    Initial Commit

---
+++ naamee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  naamee
+//
+//  Created by mac on 25/09/18.
+//  Copyright © 2018 Techmorale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ naamee/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  naamee
+//
+//  Created by mac on 25/09/18.
+//  Copyright © 2018 Techmorale. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
